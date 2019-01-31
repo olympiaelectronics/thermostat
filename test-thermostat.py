8af8a076eac1bfd2b478b74108dfdb81f00aedd7
olympiaelectronics <46441329+olympiaelectronics@users.noreply.github.com>
2019-01-31 09:01:48 +0200

    Update test-thermostat.py

--- test-thermostat.py
+++ test-thermostat.py
@@ -15,42 +15,18 @@ import urllib.request
 
 testboard = Testboard("testboard_name")
 
-# Our Product's Input will be connected the Testboard's Pin D3, making it our
-# Output Pin
-ROUTER = "D3"
-HEATER = "D2"
-FAN = "D4"
-THERMO2 = "D6"
 THERMO_ON = "D5"
-TEMPERATURE = "A4"
+THERMO2 = "D6"
 
-#get the current temperature.
-def get_temperature():
-    value = testboard.analogRead(TEMPERATURE)
-    voltage = (3.3 * value) / 4096
-    return (voltage - 0.5) * 100
-    
 def perform_test():
-    print("Starting Access Point...", flush=True)
-    testboard.digitalWrite(ROUTER, 'HIGH')
-    time.sleep(1)
     print("Starting Thermostat...", flush=True)
     testboard.digitalWrite(THERMO2, 'HIGH')
     
-    time.sleep(10)
-    print("Switch Thermostat ON", flush=True)
-    testboard.digitalWrite(THERMO_ON, 'HIGH')
-    time.sleep(1)
-    testboard.digitalWrite(THERMO_ON, 'LOW')
-
-    print("Set Temperature High...", flush=True)
-    urllib.request.urlopen("https://wismart.io/sendgcmrequest.php?message=change_setTempHigh").read()
-    
     ###### POWER CONSUMPTION ######
-    print("Measuring Power Consumption for 30 seconds...", flush=True)
+    print("Measuring Power Consumption for 40 seconds keeping LCD off...", flush=True)
     # Start measuring power consumption
     testboard.startPowerMeasurement()
-    # Measure for 5 minutes
+    # Measure for 40 seconds
     time.sleep(40)
     # Stop measuring power consumption
     testboard.stopPowerMeasurement()
@@ -58,45 +34,16 @@ def perform_test():
     print("power consumption (mA) = ","%.3f" % power, flush=True)
     Spanner.assertTrue(1)
     
-    ###### TEMPERATURE RELATED ######
-    print("get the temperature...", flush=True)
-    temperature = get_temperature()
-    print("current temperature = ","%.1f" % temperature)
-    
-    print("Heating things up...", flush=True)
-    highTemperature = temperature + 2
-    # set PIN state
-    testboard.digitalWrite(HEATER, 'HIGH')
-    time.sleep(2)
-    testboard.digitalWrite(FAN, 'HIGH')
-    cnt=0
-    while temperature < highTemperature:
-        time.sleep(2)
-        temperature = get_temperature()
-        print("temperature = ","%.1f" % temperature, flush=True)
-        cnt=cnt+1
-        if(cnt>20):
-            print("exited due to timeout")
-            break
-    
-    testboard.digitalWrite(HEATER, 'LOW')
-    
-    print("Cooling things down...", flush=True)
-    lowTemperature = temperature - 2
-    cnt=0
-    while temperature > lowTemperature:
-        time.sleep(2)
-        temperature = get_temperature()
-        print("temperature = ","%.1f" % temperature, flush=True)
-        cnt=cnt+1
-        if(cnt>20):
-            print("exited due to timeout")
-            break
-            
-    testboard.digitalWrite(FAN, 'LOW')
-    testboard.digitalWrite(ROUTER, 'LOW')
-    testboard.digitalWrite(THERMO2, 'LOW')
+    testboard.startPowerMeasurement()
+    for x in range(8):
+        print("Switch Thermostat ON "+str(x), flush=True)
+        testboard.digitalWrite(THERMO_ON, 'HIGH')
+        time.sleep(1)
+        testboard.digitalWrite(THERMO_ON, 'LOW')
+    testboard.stopPowerMeasurement()
+    power = testboard.measuredPowerConsumption()
+    print("power consumption (mA) = ","%.3f" % power, flush=True)
     Spanner.assertTrue(1)
-
+   
 if __name__ == "__main__":
     perform_test()
