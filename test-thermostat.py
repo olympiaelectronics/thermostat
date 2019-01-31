83e7e5880964f41f255cfe278918ca19d1fae5c3
olympiaelectronics <46441329+olympiaelectronics@users.noreply.github.com>
2019-01-31 11:47:54 +0200

    Update test-thermostat.py

--- test-thermostat.py
+++ test-thermostat.py
@@ -1,13 +1,3 @@
-# This example will set one of our Testboard's outputs, first to HIGH, and then
-# to LOW.
-#
-# The goal of this example is to show you how you can drive a digital input on
-# your device from the Testboard.
-#
-# In our particular example, we are only setting value and not asserting
-# anything. Of course this would never be a real world example, it's only for
-# educational purposes
-
 import time
 import Spanner
 from Testboard import Testboard
@@ -15,35 +5,15 @@ import urllib.request
 
 testboard = Testboard("testboard_name")
 
-THERMO_ON = "D5"
-THERMO2 = "D6"
-
 def perform_test():
-    print("Starting Thermostat...", flush=True)
-    testboard.digitalWrite(THERMO2, 'HIGH')
-    
-    ###### POWER CONSUMPTION ######
-    print("Measuring Power Consumption for 40 seconds keeping LCD off...", flush=True)
-    # Start measuring power consumption
-    testboard.startPowerMeasurement()
-    # Measure for 40 seconds
-    time.sleep(40)
-    # Stop measuring power consumption
-    testboard.stopPowerMeasurement()
-    power = testboard.measuredPowerConsumption()
-    print("power consumption (mA) = ","%.3f" % power, flush=True)
+    print("Set wrong password", flush=True)
+    urllib.request.urlopen("https://wismart.io/sendgcmrequest.php?message=login_wrong").read()
+    time.sleep(15)
+    print("Set correct password", flush=True)
+    urllib.request.urlopen("https://wismart.io/sendgcmrequest.php?message=login_correct").read()
+	
     Spanner.assertTrue(1)
-    
-    testboard.startPowerMeasurement()
-    for x in range(8):
-        print("Switch Thermostat ON "+str(x), flush=True)
-        testboard.digitalWrite(THERMO_ON, 'HIGH')
-        time.sleep(1)
-        testboard.digitalWrite(THERMO_ON, 'LOW')
-    testboard.stopPowerMeasurement()
-    power = testboard.measuredPowerConsumption()
-    print("power consumption (mA) = ","%.3f" % power, flush=True)
-    Spanner.assertTrue(1)
+
    
 if __name__ == "__main__":
     perform_test()
