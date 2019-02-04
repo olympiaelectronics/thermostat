5ebf955580499472b61cf1c2fbe7f85a843c4f4d
olympiaelectronics <46441329+olympiaelectronics@users.noreply.github.com>
2019-02-04 23:18:06 +0200

    Update test-thermostat.py

--- test-thermostat.py
+++ test-thermostat.py
@@ -3,17 +3,39 @@ import Spanner
 from Testboard import Testboard
 import urllib.request
 
+FAN = "D4"
 ROUTER = "D3"
-
+THERMO2 = "D6"
+THERMO_ON = "D5"
+TEMPERATURE = "A4"
 
 testboard = Testboard("testboard_name")
 
-def perform_test():
-    print("Starting Access Point and wait for a minute...", flush=True)
-    testboard.digitalWrite(ROUTER, 'HIGH')
-    time.sleep(120)
+def cool_down():
+    time.sleep(10)
    
+    testboard.digitalWrite(FAN, 'HIGH')
+    time.sleep(240)
     Spanner.assertTrue(1)
-    	
+    
 if __name__ == "__main__":
-    perform_test()
+    print("Starting Access Point...", flush=True)
+    testboard.digitalWrite(ROUTER, 'HIGH')
+    time.sleep(40)
+    print("Starting Thermostat...", flush=True)
+    testboard.digitalWrite(THERMO2, 'HIGH')
+    time.sleep(10)
+    print("Switch Thermostat ON", flush=True)
+    testboard.digitalWrite(THERMO_ON, 'HIGH')
+    time.sleep(1)
+    testboard.digitalWrite(THERMO_ON, 'LOW')
+	
+    cool_down()
+    
+    testboard.digitalWrite(THERMO_ON, 'LOW')
+    testboard.digitalWrite(ROUTER, 'LOW')
+    testboard.digitalWrite(THERMO2, 'LOW')	
+    testboard.digitalWrite(FAN, 'LOW')
+    testboard.digitalWrite(HEATER, 'LOW') 
+ 
+    Spanner.assertTrue(1)
