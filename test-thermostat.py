30e52e7696cae6a390217ab59a0b346816a14372
olympiaelectronics <46441329+olympiaelectronics@users.noreply.github.com>
2019-02-05 00:20:55 +0200

    Update test-thermostat.py

--- test-thermostat.py
+++ test-thermostat.py
@@ -9,6 +9,7 @@ ROUTER = "D3"
 THERMO2 = "D6"
 THERMO_ON = "D5"
 TEMPERATURE = "A4"
+RELAY = "D7"
 
 testboard = Testboard("testboard_name")
 
@@ -21,7 +22,7 @@ def cool_down():
 
 def cool_heatup():
     cnt=0
-    while cnt < 5:
+    while cnt < 3:
         testboard.digitalWrite(FAN, 'HIGH')
         testboard.digitalWrite(HEATER, 'HIGH')
         time.sleep(10)
@@ -42,14 +43,36 @@ if __name__ == "__main__":
     testboard.digitalWrite(THERMO_ON, 'HIGH')
     time.sleep(1)
     testboard.digitalWrite(THERMO_ON, 'LOW')
+ 
+    urllib.request.urlopen("https://wismart.io/sendgcmrequest.php?message=change_setTempLowest").read()
+    print("Wait 15 seconds...", flush=True)
+    time.sleep(15)
+
+    print("Set Temperature High...", flush=True)
+    urllib.request.urlopen("https://wismart.io/sendgcmrequest.php?message=change_setTempHigh").read()
+    print("Wait 15 seconds...", flush=True)
+    time.sleep(15)
 	
     cool_heatup()
     testboard.digitalWrite(HEATER, 'LOW')
     testboard.digitalWrite(FAN, 'LOW')	
-    time.sleep(200)
+    
+    value = testboard.digitalRead(RELAY)
+    cnt=0
+    while value != 1:
+         print("Waiting...", flush=True)		
+        time.sleep(20)
+	value = testboard.digitalRead(RELAY)		
+	cnt=cnt+1
+        if cnt == 10:
+            break
+
+    if cnt == 10:
+        Spanner.assertTrue(0)
+    else:
+        Spanner.assertTrue(1)
 	
     testboard.digitalWrite(THERMO_ON, 'LOW')
     testboard.digitalWrite(ROUTER, 'LOW')
     testboard.digitalWrite(THERMO2, 'LOW')	
-    
-    Spanner.assertTrue(1)
+   
